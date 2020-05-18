5cf0aa10c583d93a24b00a6a99da41b650f268cd
Javier Paravich <jiparavich@uc.cl>
2020-05-17 23:59:40 -0400

    s

---
+++ build/precache-manifest.99d92c8a7904e3598cf6f876adee0286.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "caec7a1167d7d5de13c1f9009bbd6cb7",
+    "url": "/Tarea3-TI/index.html"
+  },
+  {
+    "revision": "19c33930d81867a842f9",
+    "url": "/Tarea3-TI/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "461b8eabcd1567bd9456",
+    "url": "/Tarea3-TI/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "19c33930d81867a842f9",
+    "url": "/Tarea3-TI/static/js/2.104bdc0f.chunk.js"
+  },
+  {
+    "revision": "c04130d8b6692f26b622ca7bd7d5f6b5",
+    "url": "/Tarea3-TI/static/js/2.104bdc0f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "461b8eabcd1567bd9456",
+    "url": "/Tarea3-TI/static/js/main.9e01ce95.chunk.js"
+  },
+  {
+    "revision": "badb180e1e0de5e7e774",
+    "url": "/Tarea3-TI/static/js/runtime-main.fa68130f.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Tarea3-TI/static/media/logo.5d5d9eef.svg"
+  }
+]);
